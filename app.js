ea88236ab3eeae7bbfb73a4d90d93ed173974d1e
leonidSpiri <2003.leonid2003@gmail.com>
2023-03-25 00:57:55 +0300

    cool shit work with user_photo_list. now downloads all photo's json data
    work with download_photo. now can download full or thumbnail photo

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const Minio = require('minio');
 const {Pool} = require('pg');
 const fileUpload = require('express-fileupload');
 const exifImage = require('exif').ExifImage;
+const resizeImg = require('resize-image-buffer');
 
 const verifyToken = require('./controllers/verifyToken');
 
@@ -128,7 +129,7 @@ app.post('/upload', verifyToken, async function (req, res) {
             }
 
 
-            const file_location = "http://home-system.sknt.ru:2790/" + bucketName + "/" + media_id;
+            const file_location = media_id + fileExtension;
             const requestMedia = "insert into media (media_id, album_id, description, file_location, media_type, date_created, geo_location, camera_info, is_favourite, is_deleted, original_name)" +
                 " values ('" + media_id + "', '" + album_id + "', '', '" + file_location + "', '" + media_type + "', '" + timestamp + "', '" + original_geo_location + "', '" + original_camera_info + "', 'false', 'false', '" + original_name + "');";
             console.log(requestMedia)
@@ -141,7 +142,7 @@ app.post('/upload', verifyToken, async function (req, res) {
                     return
                 }
             });
-            minioClient.putObject(bucketName, media_id + fileExtension, files.file.data, files.file.size, media_type, function (err, etag) {
+            minioClient.putObject(bucketName, file_location, files.file.data, files.file.size, media_type, function (err) {
                 if (err) {
                     console.log(err)
                     res.status(500).send("Server error");
@@ -150,62 +151,113 @@ app.post('/upload', verifyToken, async function (req, res) {
                 res.status(200).send("File uploaded successfully.")
             })
         });
-
-
     });
 });
 
-app.get("/users_photo_list", verifyToken, async function (req, res) {
-    try {
-        const bucketName = "user-" + req.userTokenDecoded.user_id
-        const objectsStream = minioClient.listObjects(bucketName, '', true)
-        const photosNamesArray = []
-        objectsStream.on('data', function (obj) {
-            console.log(obj)
-            photosNamesArray.push(obj.name)
-        })
-        objectsStream.on('error', function (e) {
-            console.log(e)
-            res.status(500).send("Server error");
-        })
-        objectsStream.on('end', function () {
-            res.status(200).send(photosNamesArray)
-        })
 
+app.get("/users_photo_list/:album", verifyToken, async function (req, res) {
+    try {
+        const user_id = req.userTokenDecoded.user_id;
+        const album = req.params["album"];
+        const photosArray = []
+
+        const requestAlbum = "SELECT album_id FROM album WHERE user_id = '" + user_id.toString() + "' AND description = '" + album + "';";
+        console.log(requestAlbum)
+
+        await sql.query(requestAlbum, async function (err, resultAlbum) {
+            if (err) {
+                console.log(err);
+                res.status(500).send("Server error");
+                return
+            }
+            if (resultAlbum.rows.length === 0) {
+                res.status(404).send("Not found");
+                return
+            }
+            const album_id = resultAlbum.rows[0].album_id;
+            const requestMedia = "SELECT * FROM media WHERE album_id = '" + album_id.toString() + "';";
+            console.log(requestMedia)
+            await sql.query(requestMedia, function (err2, resultMedia) {
+                if (err2) {
+                    console.log(err2);
+                    res.status(500).send("Server error");
+                    return
+                }
+                if (resultMedia.rows.length === 0) {
+                    res.status(404).send("Not found");
+                    return;
+                }
+                for (let i = 0; i < resultMedia.rows.length; i++) {
+                    let json = {
+                        "media_id": resultMedia.rows[i].media_id,
+                        "album_id": resultMedia.rows[i].album_id,
+                        "description": resultMedia.rows[i].description,
+                        "file_location": resultMedia.rows[i].file_location,
+                        "media_type": resultMedia.rows[i].media_type,
+                        "date_created": resultMedia.rows[i].date_created,
+                        "geo_location": resultMedia.rows[i].geo_location,
+                        "camera_info": resultMedia.rows[i].camera_info,
+                        "original_name": resultMedia.rows[i].original_name,
+                        "is_favourite": resultMedia.rows[i].is_favourite,
+                        "is_deleted": resultMedia.rows[i].is_deleted
+                    }
+                    photosArray.push(json);
+                }
+                res.status(200).send(photosArray);
+            });
+
+        });
     } catch (err) {
         console.log(err);
         res.status(500).send("Server error");
     }
 });
 
-app.get("/download_photo", verifyToken, async function (req, res) {
+app.get("/download_photo/:size", verifyToken, async function (req, res) {
     try {
-        const bucketName = "user-" + req.userTokenDecoded.user_id
+
+        let fullPhoto = true
+        if (req.params["size"] === "thumbnail")
+            fullPhoto = false;
+
+        const bucketName = "user-" + req.userTokenDecoded.user_id;
         let data;
         minioClient.getObject(bucketName, req.query.fileName, function (err, objStream) {
-            if (err) {
-                return console.log(err)
+                if (err) {
+                    res.status(500).send("Server error");
+                    return console.log(err)
+                }
+                objStream.on('data', function (chunk) {
+                    data = !data ? new Buffer.from(chunk) : Buffer.concat([data, chunk]);
+                })
+                objStream.on('end', function () {
+                    if (!fullPhoto) {
+                        (async () => {
+                            const image = await resizeImg(data, {
+                                width: 200
+                            });
+                            res.write(image);
+                            res.end();
+                        })();
+                    } else {
+                        res.write(data);
+                        res.end();
+                    }
+                })
+                objStream.on('error', function (err) {
+                    res.status(500);
+                    res.send(err);
+                })
             }
-            objStream.on('data', function (chunk) {
-                data = !data ? new Buffer.from(chunk) : Buffer.concat([data, chunk]);
-            })
-            objStream.on('end', function () {
-                res.writeHead(200, {'Content-Type': 'image/jpeg'});
-                res.write(data);
-                res.end();
-            })
-            objStream.on('error', function (err) {
-                res.status(500);
-                res.send(err);
-            })
-        });
-
-
-    } catch (err) {
+        )
+        ;
+    } catch
+        (err) {
         console.log(err);
         res.status(500).send("Server error");
     }
-});
+})
+;
 
 //localhost:3000/user/registration
 app.post("/user/registration", jsonParser, async function (request, response) {
